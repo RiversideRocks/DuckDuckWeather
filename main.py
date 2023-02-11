6618a3e2cef71a7aa55af3998ab338b9cf08323e
RiversideRocks <wilest7@gmail.com>
2023-02-11 13:20:02 -0500

    more

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def weather(lat, long, time):
     return {"hourly": {"sum": r.json()["hourly"]["summary"], "image": "https://duckduckgo.com/assets/weather/icons/" + r.json()["hourly"]["icon"] + ".svg"}}
 
 def weatherMultipleDays(lat, long, startTime, count, interval):
+    # count: how many weather times to fetch
+    # interval: how much the weather times should be split apart
+    # ie. weatherMultipleDays(0, 0, int(time.time()), 5, 3600)
+    # Would get the hourly weather for five hours
     bucket = []
     for x in range(count):
         bucket.append(weather(lat, long, startTime + (interval * x)))
